6bfa624e3f52d3f0330383cd183474044eb8b580
Ashish Bhargava <ashishbharg@gmail.com>
2019-01-17 01:01:47 +0530

    Printing utility for data structures

---
+++ src/main/java/edu/ashish/util/PrintingUtil.java
@@ -0,0 +1,12 @@
+package edu.ashish.util;
+
+public class PrintingUtil {
+
+    public static void print1DArray(int[] arr) {
+
+        for(int num: arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
