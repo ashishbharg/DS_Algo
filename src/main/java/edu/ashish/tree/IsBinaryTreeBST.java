d318db6a79e270a1ee34579b92bea0795f644186
Ashish Bhargava <ashishbharg@gmail.com>
2019-01-20 10:53:04 +0530

    Program to find if binary tree is BST or not

---
+++ src/main/java/edu/ashish/tree/IsBinaryTreeBST.java
@@ -0,0 +1,45 @@
+package edu.ashish.tree;
+
+import edu.ashish.model.TreeNode;
+import edu.ashish.util.DSCreationUtil;
+
+/**
+ * Program to find out if binary tree is BST or not.
+ */
+public class IsBinaryTreeBST {
+
+    public static void main(String[] args) {
+
+        TreeNode root = DSCreationUtil.createBinaryTree();
+
+        boolean isBinaryBST = isBinaryBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        print(isBinaryBST);
+
+        root.left.right = new TreeNode(25);
+        isBinaryBST = isBinaryBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        print(isBinaryBST);
+
+    }
+
+    private static boolean isBinaryBST(TreeNode root, int min, int max) {
+
+        if (root == null) {
+            return true;
+        }
+
+        if(root.getData() <= min || root.getData() > max) {
+            return false;
+        }
+
+        return isBinaryBST(root.left, min, root.getData()) &&
+                isBinaryBST(root.right, root.getData(), max);
+    }
+
+    private static void print(boolean isBinaryBST) {
+        if (isBinaryBST) {
+            System.out.println("Binary Tree is BST");
+        } else {
+            System.out.println("Binary Tree is not BST");
+        }
+    }
+}
