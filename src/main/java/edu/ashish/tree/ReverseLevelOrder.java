bae5cb6454511958c3f73e5843ec41509525a1e3
Ashish Bhargava <ashishbharg@gmail.com>
2019-01-20 14:44:00 +0530

    Printing reverse level order

---
+++ src/main/java/edu/ashish/tree/ReverseLevelOrder.java
@@ -0,0 +1,43 @@
+package edu.ashish.tree;
+
+import edu.ashish.model.TreeNode;
+import edu.ashish.util.DSCreationUtil;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+/**
+ * Program to print level order traversal in reverse order.
+ */
+
+public class ReverseLevelOrder {
+
+    public static void main(String[] args) {
+        TreeNode root = DSCreationUtil.createBinaryTree();
+        reverseLevelOrder(root);
+    }
+
+    private static void reverseLevelOrder(TreeNode root) {
+
+        Stack<TreeNode> stack = new Stack<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            stack.push(node);
+        }
+
+        while (!stack.empty()) {
+            System.out.print(stack.pop().getData() + " ");
+        }
+    }
+}
